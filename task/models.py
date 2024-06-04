e7014743ca4cbc7a436d3d1bce12c8767b680738
ainazik <ainazikbaltabaeva@gmail.com>
2024-06-04 11:33:46 +0600

    first save, start project

---
+++ task/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
